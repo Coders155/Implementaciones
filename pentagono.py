3dd8b6f8471260fca6e9137b5f9b264e9284c918
TenorioMartínezLeonardo <119890042+DowncastList1@users.noreply.github.com>
2023-05-29 21:20:38 -0600

    Add files via upload

---
+++ pentagono.py
@@ -0,0 +1,11 @@
+A=int(input("Escribe la Altura: "))
+bama=int(input("Escribe la base menor: "))
+espacios=0
+
+for i in range (A):
+    espacios=A-i
+    print(" "*espacios+"* "*(i+1))
+for i in range (A-3): 
+    espacios=espacios+1
+    print(" "*espacios+'* '*bama)
+    bama=bama-1
---
+++ triequi.py
@@ -0,0 +1,4 @@
+A=int(input("Escribe la medida: "))
+for i in range (A):
+    espacios=A-i
+    print(" "*espacios+"* "*(i+1))
